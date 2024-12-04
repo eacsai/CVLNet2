9667096962cde7d0b9334f007d1d46a90f18f57e
eacsai <1349157283@qq.com>
2024-12-04 11:28:43 +0800

    fix: fix lpips and learning rate

--- kitti_image_model_plan2_gaussian.py
+++ kitti_image_model_plan2_gaussian.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 from jaxtyping import Float
 from torch import Tensor
-
+from lpips import LPIPS
 from gaussian.build_gaussians import build_covariance, map_pdf_to_opacity
 # from cuda_splatting import render_cuda
 from nopo_cuda_splatting import render_cuda
@@ -26,10 +26,10 @@ from depth_predictor.depth_predictor_monocular import DepthPredictorMonocular
 from gaussian.build_gaussians import *
 from gaussian.gaussian_adapter import get_world_rays, rotate_sh
 from vis_gaussian import render_projections
+from loss.lpips import convert_to_buffer
 
 to_pil_image = transforms.ToPILImage()
-
-deterministic = False
+Lpips_step = 15000
 
 def print_grad(grad):
     print("Gradient shape:", grad.shape)
@@ -44,6 +44,7 @@ class Gaussians:
 class Model(nn.Module):
     def __init__(self, args, direct_map = False, use_bn = False):  # device='cuda:0',
         super(Model, self).__init__()
+        self.global_step = 0
         self.args = args
         self.d_sh = 25
         self.backbone = BackboneDino()
@@ -82,7 +83,8 @@ class Model(nn.Module):
         )
         for degree in range(1, 25 + 1):
             self.sh_mask[degree**2 : (degree + 1) ** 2] = 0.1 * 0.25**degree
-        
+        self.lpips = LPIPS(net="vgg")
+        convert_to_buffer(self.lpips, persistent=False)
 
     def forward_project(self, image_tensor, camera_k, depth, ori_grdH=256, ori_grdW=1024):
         image_rgb = image_tensor.clone()
@@ -131,38 +133,17 @@ class Model(nn.Module):
 
         return xyz_grd, depth
     
-    def forward_project_v2(self, coordinates, camera_k, depth, ori_grdH=256, ori_grdW=1024):
-        B, grd_H, grd_W, _ = coordinates.shape
-        camera_k = camera_k.clone()
-        camera_k[:, :1, :] = camera_k[:, :1,
-                                :] * grd_W / ori_grdW  # original size input into feature get network/ output of feature get network
-        camera_k[:, 1:2, :] = camera_k[:, 1:2, :] * grd_H / ori_grdH
-        # meter_per_pixel = 1
-        # with torch.no_grad():
-        camera_k_inv = torch.inverse(camera_k)
-        # camera_k_inv = torch.inverse(camera_k)  # [B, 3, 3]
-
-        # uv1 = torch.cat([coordinates, torch.ones_like(coordinates[..., :1])], dim=-1)
-        # uv1 = rearrange(uv1, "b h w xyz -> b (h w) xyz")
-        xyz_w = torch.sum(camera_k_inv[:, None, None, :, :] * coordinates[:, :, :, None, :], dim=-1)  # [1, grd_H, grd_W, 3]
-
-        depth = depth.unsqueeze(-1)
-        depth = F.interpolate(depth.permute(0,3,1,2), size=(grd_H, grd_W), mode='bilinear', align_corners=False).permute(0,2,3,1)
-        # xyz_grd = xyz_w * depth / meter_per_pixel
-        xyz_grd = xyz_w
-        return xyz_grd, depth
-    
-    def gaussian_map(self, sat_map, grd_img_left, project_map, grd_depth, left_camera_k, gt_shift_u=None, gt_shift_v=None, gt_heading=None, mode='train'):
+    def gaussian_init(self, sat_map, grd_img_left, project_map, grd_depth, left_camera_k, gt_shift_u=None, gt_shift_v=None, gt_heading=None, mode='train'):
         # initial
-        camera_k = left_camera_k.clone()
-        camera_k[:, :1, :] = camera_k[:, :1, :] / 1024  # original size input into feature get network/ output of feature get network
-        camera_k[:, 1:2, :] = camera_k[:, 1:2, :] / 256
+        self.camera_k = left_camera_k.clone()
+        self.camera_k[:, :1, :] = self.camera_k[:, :1, :] / grd_depth.shape[2]  # original size input into feature get network/ output of feature get network
+        self.camera_k[:, 1:2, :] = self.camera_k[:, 1:2, :] / grd_depth.shape[1]
         
         grd_depth = grd_depth.unsqueeze(-1)
-        grd_depth = F.interpolate(grd_depth.permute(0,3,1,2), size=(128, 512), mode='bilinear', align_corners=False).permute(0,2,3,1)
+        self.grd_depth = F.interpolate(grd_depth.permute(0,3,1,2), size=(128, 512), mode='bilinear', align_corners=False).permute(0,2,3,1)
         grd_img_left = F.interpolate(grd_img_left, size=(128, 512), mode='bilinear', align_corners=False)
-        B, _, ori_grdH, ori_grdW = grd_img_left.shape
-        extrinsics = torch.eye(4).to(grd_img_left.device).unsqueeze(0).repeat(B, 1, 1)
+        B, _, self.ori_grdH, self.ori_grdW = grd_img_left.shape
+        self.extrinsics = torch.eye(4).to(grd_img_left.device).unsqueeze(0).repeat(B, 1, 1)
         
         # Encode the context images.
         grd_feat = self.backbone(grd_img_left)
@@ -174,38 +155,30 @@ class Model(nn.Module):
         grd_feat = grd_feat + skip
 
         # Sample depths from the resulting features.
-        grd_feat = rearrange(grd_feat, "b c h w -> b (h w) c").contiguous()  
-        near = torch.ones(B).to(grd_feat.device) * 0.5
-        far = torch.ones(B).to(grd_feat.device) * 100
+        self.grd_feat = rearrange(grd_feat, "b c h w -> b (h w) c").contiguous()  
+        self.near = torch.ones(B).to(grd_feat.device) * 0.5
+        self.far = torch.ones(B).to(grd_feat.device) * 100
+        self.grd_img_left = grd_img_left
+
+    def gaussian_map(self, deterministic=False):
         depths, densities = self.depth_predictor.forward(
-            grd_feat,
-            near,
-            far,
+            self.grd_feat,
+            self.near,
+            self.far,
             deterministic,
             1 if deterministic else 3,
         )
 
         # Convert the features and depths into Gaussians.
-        xy_ray, _ = sample_image_grid((ori_grdH, ori_grdW), grd_feat.device)
+        xy_ray, _ = sample_image_grid((self.ori_grdH, self.ori_grdW), self.grd_feat.device)
         xy_ray = rearrange(xy_ray, "h w xy -> (h w) () xy").contiguous()
-        gaussians = self.to_gaussians(grd_feat).unsqueeze(-2)
+        gaussians = self.to_gaussians(self.grd_feat).unsqueeze(-2)
         # gaussians = rearrange(gaussians, "b (h w) c -> b h w c", h=ori_grdH, w=ori_grdW)
         offset_xy = gaussians[..., :2].sigmoid()
-        pixel_size = 1 / torch.tensor((ori_grdW, ori_grdH), dtype=torch.float32, device=grd_feat.device)
+        pixel_size = 1 / torch.tensor((self.ori_grdW, self.ori_grdH), dtype=torch.float32, device=self.grd_feat.device)
         coordinates = xy_ray + (offset_xy - 0.5) * pixel_size
-        # v, u = torch.meshgrid(torch.arange(0, ori_grdH, dtype=torch.float32, device=grd_feat.device),
-        #                       torch.arange(0, ori_grdW, dtype=torch.float32, device=grd_feat.device))
-        # coordinates[..., 0] = coordinates[..., 0] * 1
-        # coordinates[..., 1] = coordinates[..., 1] * 1
-
-        # coordinates = rearrange(coordinates, "b h w xy -> b (h w) xy", h=ori_grdH, w=ori_grdW)
-        # u = coordinates[..., 0]
-        # v = coordinates[..., 1]
-        # coordinates = rearrange(coordinates, "b (h w) xy -> b h w xy", h=ori_grdH, w=ori_grdW)
-        # densities = gaussians[..., 0].sigmoid().unsqueeze(-1)
-
-        # coordinates = torch.stack([u, v, torch.ones_like(u, device=grd_feat.device)], dim=-1)
-        opacities = self.map_pdf_to_opacity(densities) / 3
+        gpp = 1 if deterministic else 3
+        opacities = self.map_pdf_to_opacity(densities) / gpp
         raw_gaussians = gaussians[..., 2:]
         scales, rotations, sh = raw_gaussians.split((3, 4, 3 * self.d_sh), dim=-1)
 
@@ -213,24 +186,19 @@ class Model(nn.Module):
         scale_min = 0.5
         scale_max = 15.0
         scales = scale_min + (scale_max - scale_min) * scales.sigmoid()
-        multiplier = self.get_scale_multiplier(camera_k, pixel_size)
+        multiplier = self.get_scale_multiplier(self.camera_k, pixel_size)
         scales = scales * depths[..., None] * multiplier[..., None, None, None]
-
-        # scales = 0.001 * F.softplus(scales)
-        # scales = scales.clamp_max(0.3)
-
         # scales = scales * depths * multiplier[:, None, None, None]
         # Compute Gaussian means.
-        origins, directions = get_world_rays(coordinates, extrinsics[:, None, None, :, :], camera_k[:, None, None, :, :])
+        origins, directions = get_world_rays(coordinates, self.extrinsics[:, None, None, :, :], self.camera_k[:, None, None, :, :])
         # origins = rearrange(origins, "b (h w) xyz -> b h w xyz", h=ori_grdH, w=ori_grdW)
         grd_pts3d = directions * depths[..., None]
 
         # grd_pts3d, gt_depth = self.forward_project_v2(coordinates, left_camera_k, depths.squeeze(-1))
 
-
         # Normalize the quaternion features to yield a valid quaternion.
         rotations = rotations / (rotations.norm(dim=-1, keepdim=True) + 1e-8)
-        c2w_rotations = extrinsics[..., :3, :3]
+        c2w_rotations = self.extrinsics[..., :3, :3]
         sh = rearrange(sh, "... (xyz d_sh) -> ... xyz d_sh", xyz=3).contiguous()
         # TODO: handle the harmonics
         sh = sh.broadcast_to((*depths.shape, 3, self.d_sh)) * self.sh_mask.to(sh.device)
@@ -238,19 +206,7 @@ class Model(nn.Module):
         covariances = build_covariance(scales, rotations)
         rotations = rotations.broadcast_to((*scales.shape[:-1], 4))
 
-        background_color = torch.zeros(B, 3).float().to(grd_pts3d.device)
-        # color = render_cuda(
-        #     extrinsics,
-        #     camera_k,
-        #     near,
-        #     far,
-        #     (128, 512),
-        #     background_color,
-        #     final_gaussians.means,
-        #     final_gaussians.covariances,
-        #     final_gaussians.harmonics,
-        #     final_gaussians.opacities,
-        # )
+        background_color = torch.zeros(self.grd_feat.shape[0], 3).float().to(grd_pts3d.device)
         final_gaussians = Gaussians(
             rearrange(
                 grd_pts3d,
@@ -270,10 +226,10 @@ class Model(nn.Module):
             ).contiguous()
         )
         color, depth = render_cuda(
-            extrinsics,
-            camera_k,
-            near,
-            far,
+            self.extrinsics,
+            self.camera_k,
+            self.near,
+            self.far,
             (128, 512),
             background_color,
             final_gaussians.means,
@@ -282,46 +238,64 @@ class Model(nn.Module):
             final_gaussians.opacities,
             scale_invariant=True,
         )
-        # color.register_hook(print_grad)       
-        rgb_mse_loss = F.mse_loss(color, grd_img_left, reduction='mean')
-        depth_l1_loss = F.l1_loss(depth.unsqueeze(-1), grd_depth, reduction='mean')
-        loss = rgb_mse_loss * 50 + depth_l1_loss
-        test_img = to_pil_image(color[0].clip(min=0, max=1))
-        test_img.save('test.png')
-        test_img = to_pil_image(grd_img_left[0])
-        test_img.save('gt.png')
-        render_projections(final_gaussians, (256,256), extra_label='test')
-        # ply_path = Path(f"test.ply")
-        # visualization_dump={}
-        # visualization_dump["scales"] = scales.reshape(B,-1,3)
-        # visualization_dump["rotations"] = rotations.reshape(B,-1,4)
-        # export_ply(
-        #     torch.eye(4),
-        #     final_gaussians.means[0],
-        #     visualization_dump["scales"][0],
-        #     visualization_dump["rotations"][0],
-        #     final_gaussians.harmonics[0],
-        #     final_gaussians.opacities[0],
-        #     ply_path,
-        # )
+        # color.register_hook(print_grad)   
+        if not deterministic:    
+            rgb_mse_loss = F.mse_loss(color, self.grd_img_left, reduction='mean')
+            depth_l1_loss = F.l1_loss(depth.unsqueeze(-1), self.grd_depth, reduction='mean')
+            if self.global_step > Lpips_step:
+                lpips_loss = self.lpips.forward(
+                    color,
+                    self.grd_img_left,
+                    normalize=True,
+                )
+            else:
+                # lpips_loss = self.lpips.forward(
+                #     color,
+                #     self.grd_img_left,
+                #     normalize=True,
+                # )
+                lpips_loss = torch.tensor(0, dtype=torch.float32, device=color.device)
+            loss = rgb_mse_loss * 40 + depth_l1_loss + lpips_loss.mean() * 2
+            test_img = to_pil_image(color[0].clip(min=0, max=1))
+            test_img.save('probabilistic_test.png')
+            test_img = to_pil_image(self.grd_img_left[0])
+            test_img.save('gt.png')
+            render_projections(final_gaussians, (256,256), extra_label='probabilistic_test')
+            # ply_path = Path(f"test.ply")
+            # visualization_dump={}
+            # visualization_dump["scales"] = scales.reshape(B,-1,3)
+            # visualization_dump["rotations"] = rotations.reshape(B,-1,4)
+            # export_ply(
+            #     torch.eye(4),
+            #     final_gaussians.means[0],
+            #     visualization_dump["scales"][0],
+            #     visualization_dump["rotations"][0],
+            #     final_gaussians.harmonics[0],
+            #     final_gaussians.opacities[0],
+            #     ply_path,
+            # )
 
-        # # 旋转矩阵 (绕 x 轴旋转 -90°)
-        # rotation = torch.tensor([
-        #     [1, 0,  0],
-        #     [0, 0, -1],
-        #     [0, 1,  0]
-        # ], dtype=torch.float32)
+            # # 旋转矩阵 (绕 x 轴旋转 -90°)
+            # rotation = torch.tensor([
+            #     [1, 0,  0],
+            #     [0, 0, -1],
+            #     [0, 1,  0]
+            # ], dtype=torch.float32)
 
-        # # 平移向量 (0, 0, 0)
-        # translation = torch.tensor([0, 0, 0], dtype=torch.float32)
+            # # 平移向量 (0, 0, 0)
+            # translation = torch.tensor([0, 0, 0], dtype=torch.float32)
 
-        # # 构造齐次变换矩阵
-        # extrinsics = torch.eye(4, dtype=torch.float32)
-        # extrinsics[:3, :3] = rotation
-        # extrinsics[:3, 3] = translation
-        # extrinsics = extrinsics.unsqueeze(0).repeat(B, 1, 1)
+            # # 构造齐次变换矩阵
+            # extrinsics = torch.eye(4, dtype=torch.float32)
+            # extrinsics[:3, :3] = rotation
+            # extrinsics[:3, 3] = translation
+            # extrinsics = extrinsics.unsqueeze(0).repeat(B, 1, 1)
+            return loss
+        else:
+            test_img = to_pil_image(color[0].clip(min=0, max=1))
+            test_img.save('deterministic_test.png')
+            render_projections(final_gaussians, (256,256), extra_label='deterministic_test')
 
-        return loss
     def get_scale_multiplier(
         self,
         intrinsics: Float[Tensor, "*#batch 3 3"],
--- train_plan2_gaussian_map.py
+++ train_plan2_gaussian_map.py
@@ -18,7 +18,8 @@ def parse_args():
     parser.add_argument('--name', type=str, default='gaussian_map')
     parser.add_argument('--epochs', type=int, default=100) 
     parser.add_argument('--batch_size', type=int, default=6)
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--warm_up_steps', type=float, default=2000)    
     parser.add_argument('--level', type=int, default=3, help='2, 3, 4, -1, -2, -3, -4')
     parser.add_argument('--rotation_range', type=float, default=0., help='degree')
     parser.add_argument('--shift_range_lat', type=float, default=20., help='meters')
@@ -45,8 +46,14 @@ def train(model, lr, args, save_path):
         
         base_lr = lr
         base_lr = base_lr * ((1.0 - float(epoch) / 100.0) ** (1.0))
-
         optimizer = optim.Adam(model.parameters(), lr=base_lr)
+        scheduler = torch.optim.lr_scheduler.LinearLR(
+            optimizer,
+            start_factor=1 / args.warm_up_steps,
+            end_factor=1.0,
+            total_iters=args.warm_up_steps,
+        )
+
         optimizer.zero_grad()
 
         train_loader = load_train_data(mini_batch, root=args.root)
@@ -56,19 +63,23 @@ def train(model, lr, args, save_path):
             sat_map, left_camera_k, grd_left_imgs, gt_shift_u, gt_shift_v, gt_heading, grd_height, project_map, sat_height, grd_depth = [item.to(device) for item in Data[:-1]]
 
             optimizer.zero_grad()
-
             # corr_loss, mse_loss = model.feature_map(sat_map, grd_left_imgs, grd_depth, left_camera_k, gt_shift_u, gt_shift_v, gt_heading, mode='train')
             # loss = corr_loss + mse_loss
-            loss = model.gaussian_map(sat_map, grd_left_imgs, project_map, grd_depth, left_camera_k, gt_shift_u, gt_shift_v, gt_heading, mode='train')
+            model.gaussian_init(sat_map, grd_left_imgs, project_map, grd_depth, left_camera_k, gt_shift_u, gt_shift_v, gt_heading, mode='train')
+            loss = model.gaussian_map()
             loss.backward()
-            # model.global_step = model.global_step + sat_map.shape[0]
             # 打印每个参数的梯度
             # for name, param in model.named_parameters():
             #     print(f"Parameter: {name}, Gradient: {param.shape}")
             optimizer.step()
             optimizer.zero_grad()
 
+            # 更新学习率
+            scheduler.step()
+            model.global_step = model.global_step + sat_map.shape[0]
+
             if Loop % 10 == 9:  #
+                model.gaussian_map(deterministic=True)
                 print('Epoch: ' + str(epoch) + ' Loop: ' + str(Loop) + ' Loss: ' + str(loss.item()))
 
         print('Save Model ...')
